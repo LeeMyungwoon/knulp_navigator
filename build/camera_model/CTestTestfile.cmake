e978bbda05f84b602ab47a2a48a5441fc7a7ef0f
LeeMyungwoon <knulpmyung@gmail.com>
2025-11-19 12:45:22 +0900

    Clean build artifacts and auto-generated makefiles

--- build/camera_model/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/knulp/dev_ws/knulp_navigator/camera_model
-# Build directory: /home/knulp/dev_ws/knulp_navigator/build/camera_model
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/feature_tracker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/knulp/dev_ws/knulp_navigator/feature_tracker
-# Build directory: /home/knulp/dev_ws/knulp_navigator/build/feature_tracker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
